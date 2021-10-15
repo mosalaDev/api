e1e04f1af4061a6e82f1597232e91afc551ecb96
samkindev <samkyunyudev@gmail.com>
2021-10-15 08:57:29 -0700

    Some api modifications

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,21 @@ router.post('/refresh_token', authControllers.refresh_token);
 
 router.delete('/logout', authControllers.logout_user);
 
+router.get('/', (req, res, next) => {
+    try {
+        const user = req.user;
+
+        if (!user) {
+            return res.json({
+                code: 'user/unauthenticated',
+                message: 'aucun utilisateur connecté'
+            });
+        }
+
+        return res.json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = router;
